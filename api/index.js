4c869a02ca88224e27526b736a60b44dbedf57a4
JONEL <jonelteano29@gmail.com>
2024-04-30 19:47:22 +0800

    test module type

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import dotenv from "dotenv";
 
-// DATABASE CONNECTION
-import { initDatabase } from './database.js'
+// // DATABASE CONNECTION
+// import { initDatabase } from './database.js'
 
 const app = express();
-dotenv.config();      // ACCESS .ENV 
-initDatabase();
+// dotenv.config();      // ACCESS .ENV 
+// initDatabase();
 
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
