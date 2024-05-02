f0bb249b0544f7c61f7ab491d6043f6c968be604
JONEL <jonelteano29@gmail.com>
2024-05-02 13:35:23 +0800

    configure cors for my frontend fetching

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
-
+import cors from 'cors';
 
 
 // DATABASE CONNECTION
@@ -14,10 +14,26 @@ const app = express();
 dotenv.config();      // ACCESS .ENV 
 initDatabase();
 
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'http://localhost:3001/Home',
+        'https://reward-funding-website.vercel.app/',
+        'https://reward-funding-website.vercel.app/Home'
+    ],  // THE HTTP(ORIGIN) THAT WILL ALLOW TO ACCESS THE ROUTES
+    credentials: true,
+}));
+
+
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
+
 
 // USE ROUTES
 app.use('/api', userRoutes);
